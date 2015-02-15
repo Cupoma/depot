12180ffcd36f5c31fa35a0b2bbc25d5e83e9bf40
Taras Sirota <taras.sirota@gmail.com>
2015-02-15 22:27:03 +0200

    page 189

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -26,17 +26,20 @@ class LineItemsController < ApplicationController
   # POST /line_items
   # POST /line_items.json
   def create
+    @cart = current_cart
+
     product = Product.find(params[:product_id])
     @line_item = @cart.add_product(product.id)
-
     respond_to do |format|
       if @line_item.save
         format.html { redirect_to store_url }
         format.js { @current_item = @line_item }
-        format.json { render :show, status: :created, location: @line_item }
+        format.json { render action: 'show',
+                             status: :created, location: @line_item }
       else
-        format.html { render :new }
-        format.json { render json: @line_item.errors, status: :unprocessable_entity }
+        format.html { render action: 'new' }
+        format.json { render json: @line_item.errors,
+                             status: :unprocessable_entity }
       end
     end
   end
